7abe9e695bacd4bb367fa5c0f1a0d97b53148397
Benjamin Kurz <benjamin.kurz@liebenzell.org>
2017-05-17 22:15:50 -0400

    [FEATURE] Custom RequestHandler to allow a logged in user to see the frontend regardless of pageUnavailable_force

---
+++ Classes/UserFunctions.php
@@ -0,0 +1,20 @@
+<?php
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+class UserFunctions {
+    /**
+     * @param $param
+     * @param $ref
+     */
+    public function preprocessRequest($param, $ref) {
+        // TODO Implementation
+    }
+
+    /**
+     * @param $param
+     * @param $ref
+     */
+    public function pageUnavailable($param, $ref) {
+        // TODO Implementation
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,8 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] = 'EXT:bk_maintenance/Classes/UserFunctions.php:UserFunctions->preprocessRequest';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\Http\\RequestHandler'] = array(
+    'className' => 'B84k\\BkMaintenance\\Xclass\\RequestHandler'
+);
